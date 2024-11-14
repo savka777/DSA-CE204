e0ce1100b8e185948bed6df73693c249ada22709
savka777 <sav.bojko@gmail.com>
2024-11-14 17:22:58 +0000

    Selection Sort

--- Algorithms/Main.java
+++ Algorithms/Main.java
@@ -1,13 +1,15 @@
 package Algorithms;
+import static Algorithms.InsertionSort.insertionSort;
 import static Algorithms.SelectionSort.selectionSort;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr = {4,1,2,7,5};
 
-        selectionSort(arr);
+//        selectionSort(arr);
+        insertionSort(arr);
 
-        for(int i : arr){
+        for(Integer i: arr){
             System.out.println(i);
         }
 
