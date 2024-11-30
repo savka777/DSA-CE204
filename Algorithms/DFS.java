32c8e01b5987b9f2bcf36cc6757100ddeae4ff50
savka777 <sav.bojko@gmail.com>
2024-11-30 18:23:12 +0000

    DFS added in algorithims

---
+++ Algorithms/DFS.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+import Graph.Edge;
+import Graph.Vertex;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+public class DFS {
+    public static void dfs(Vertex start) {
+        Stack<Vertex> stack = new Stack<>();
+        HashSet<Vertex> visited = new HashSet<>();
+
+        stack.push(start);
+        visited.add(start);
+
+        while (!stack.isEmpty()) {
+            Vertex curr = stack.pop();
+            System.out.println(curr.getData());
+
+            for (Edge e : curr.getEdges()) {
+                Vertex neighbor = e.getEnd();
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+}
